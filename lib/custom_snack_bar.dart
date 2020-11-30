b8b22e0e564cba2fb26fb6bfe260e7a13ef6765b
Alexey Podolian <alexey@lanars.com>
2020-11-30 09:21:14 +0200

    Improve UI for custom snack bar widget

--- lib/custom_snack_bar.dart
+++ lib/custom_snack_bar.dart
@@ -1,13 +1,74 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CustomSnackBar extends StatefulWidget {
   final String message;
+  final Widget icon;
+  final Color backgroundColor;
+  final TextStyle textStyle;
+  final int iconRotationAngle;
 
   CustomSnackBar({
     Key key,
-    this.message,
+    @required this.message,
+    @required this.icon,
+    @required this.backgroundColor,
+    this.iconRotationAngle = 32,
+    this.textStyle,
   }) : super(key: key);
+
+  const CustomSnackBar.success({
+    Key key,
+    this.message,
+    this.icon = const Icon(
+      Icons.sentiment_very_satisfied,
+      color: const Color(0x15000000),
+      size: 120,
+    ),
+    this.textStyle = const TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 16,
+      color: Colors.white,
+    ),
+    this.iconRotationAngle = 32,
+    this.backgroundColor = const Color(0xff00E676),
+  });
+
+  const CustomSnackBar.info({
+    Key key,
+    this.message,
+    this.icon = const Icon(
+      Icons.sentiment_neutral,
+      color: const Color(0x15000000),
+      size: 120,
+    ),
+    this.textStyle = const TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 16,
+      color: Colors.white,
+    ),
+    this.iconRotationAngle = 32,
+    this.backgroundColor = const Color(0xff2196F3),
+  });
+
+  const CustomSnackBar.error({
+    Key key,
+    this.message,
+    this.icon = const Icon(
+      Icons.error_outline,
+      color: const Color(0x15000000),
+      size: 120,
+    ),
+    this.textStyle = const TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 16,
+      color: Colors.white,
+    ),
+    this.iconRotationAngle = 32,
+    this.backgroundColor = const Color(0xffff5252),
+  });
 
   @override
   _CustomSnackBarState createState() => _CustomSnackBarState();
@@ -18,23 +79,50 @@ class _CustomSnackBarState extends State<CustomSnackBar> {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Container(
-      padding: EdgeInsets.all(16),
+      height: 80,
       decoration: BoxDecoration(
-        color: Colors.redAccent,
-        borderRadius: BorderRadius.all(Radius.circular(16)),
+        color: widget.backgroundColor,
+        borderRadius: BorderRadius.all(Radius.circular(12)),
         boxShadow: [
           BoxShadow(
-            color: Colors.grey,
+            color: Colors.black26,
             offset: Offset(0.0, 8.0),
-            blurRadius: 20.0,
+            spreadRadius: 1,
+            blurRadius: 30,
           ),
         ],
       ),
       width: double.infinity,
-      child: Text(
-        widget.message,
-        style: theme.textTheme.bodyText2,
-        textAlign: TextAlign.center,
+      child: Stack(
+        children: [
+          Positioned(
+            top: -10,
+            left: -8,
+            child: ClipRRect(
+              child: Container(
+                height: 95,
+                child: Transform.rotate(
+                  angle: widget.iconRotationAngle * pi / 180,
+                  child: widget.icon,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Text(
+                widget.message,
+                style: theme.textTheme.bodyText2.merge(
+                  widget.textStyle ?? TextStyle(),
+                ),
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }

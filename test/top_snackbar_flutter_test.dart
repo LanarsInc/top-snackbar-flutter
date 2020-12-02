0fdc14f8612463dd8fcf0955a47ee149237a8a53
Alexey Podolian <alexey@lanars.com>
2020-12-02 10:13:18 +0200

    Remove unneeded generated file

--- test/top_snackbar_flutter_test.dart
+++ test/top_snackbar_flutter_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:top_snackbar_flutter/top_snackbar_flutter.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('top_snackbar_flutter');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await TopSnackbarFlutter.platformVersion, '42');
   });
 }
